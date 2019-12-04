9c75a60ee8f427f17fd37ae4f60dbf875de538f2
F74066187 <dio23310045123@gmail.com>
2019-12-04 17:44:52 +0800

    monkey patch problems

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+
 import os
 import sys
+
+from fsm import TocMachine
+from utils import send_text_message
 
 from flask import Flask, jsonify, request, abort, send_file
 from dotenv import load_dotenv
@@ -7,8 +11,6 @@ from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
-from fsm import TocMachine
-from utils import send_text_message
 
 load_dotenv()
 
